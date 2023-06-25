1d0be6d9b9f0ad26c8887eaeec6d837cbba5c41d
Asorkuenu <Asorkuenu@gmail.com>
2023-06-25 10:24:34 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/", methods=["get","post"])
+def reply():
+    response = MessagingResponse()
+    response.message("Hello")
+    return str(response)
+
+if __name__ =="__main__":
+    app.run()
