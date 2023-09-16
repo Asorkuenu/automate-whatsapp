9a2eb3ac4054b698cde47f4c960283db26eb754c
Asorkuenu <Asorkuenu@gmail.com>
2023-09-16 08:43:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,10 @@ def reply():
 
         if option == 1:
             res["reply"] += '\n' + ("Our Services:\n\n"
-                                    "1️⃣ Guarding\n"
-                                    "2️⃣ Security Technologies Integration\n"
-                                    "3️⃣ Private Investigations\n"
-                                    "4️⃣ Valuables-in-Transit\n"
-                                    "Price for our services depends on assessment. 🕵️‍♂️")
+                                    "1️⃣ Guarding Services\n"
+                                    "2️⃣ Security Technologies Integration Services\n"
+                                    "3️⃣ Private Investigations Services\n"
+                                    "4️⃣ Valuables-in-Transit Services")
         elif option == 2:
             res["reply"] += '\n' + ("Contact Offin Security Services Ltd.\n\n"
                                     "📞 Tel: 020-359-8142\n"
@@ -42,6 +41,48 @@ def reply():
                                     "🌐 Website: www.offinsecuritygh.com")
         else:
             res["reply"] += '\n' + ("Please enter a valid response")
+    elif user["status"] == "main":
+        try:
+            option = int(text)
+        except:
+            res["reply"] += '\n' + ("Please enter a valid response")
+            return str(res)
+
+        if option == 1:
+            res["reply"] += '\n' + ("Our Services:\n\n"
+                                    "1️⃣ Guarding Services\n"
+                                    "2️⃣ Security Technologies Integration Services\n"
+                                    "3️⃣ Private Investigations Services\n"
+                                    "4️⃣ Valuables-in-Transit Services")
+        elif option == 2:
+            res["reply"] += '\n' + ("Contact Offin Security Services Ltd.\n\n"
+                                    "📞 Tel: 020-359-8142\n"
+                                    "📧 Email: info@offinsecuritygh.com\n"
+                                    "🌐 Website: www.offinsecuritygh.com")
+        else:
+            res["reply"] += '\n' + ("Please enter a valid response")
+    elif user["status"] == "services":
+        try:
+            option = int(text)
+        except:
+            res["reply"] += '\n' + ("Please enter a valid response")
+            return str(res)
+
+        if option == 1:
+            res["reply"] += '\n' + ("**Guarding Services:**\n\n"
+                                    "Our Guarding Services have earned the trust of notable clients including BOST, GCB, SSNIT, GRIDCo, KATH, GIPC, and Omni Energy. With a team of highly trained security personnel, we provide top-tier protection and surveillance, ensuring the safety and security of your assets and premises 24/7.")
+        elif option == 2:
+            res["reply"] += '\n' + ("**Security Technologies Integration Services:**\n\n"
+                                    "Our Security Technologies Integration Services offer cutting-edge solutions to enhance your security infrastructure. We seamlessly integrate advanced security technologies to provide real-time monitoring, access control, and threat detection. With our expertise, you can achieve unmatched security and peace of mind.")
+        elif option == 3:
+            res["reply"] += '\n' + ("**Private Investigations Services:**\n\n"
+                                    "Our Private Investigations Services provide discreet and professional solutions to uncover the truth. Whether it's corporate fraud, background checks, or personal matters, our experienced investigators employ industry-leading techniques to deliver accurate and confidential results.")
+        elif option == 4:
+            res["reply"] += '\n' + ("**Valuables-in-Transit Services:**\n\n"
+                                    "Our Valuables-in-Transit Services ensure the safe and secure transport of your valuable assets. With a proven track record, we offer comprehensive logistics solutions, armed escorts, and state-of-the-art security measures to safeguard your valuables throughout their journey. Your assets are in trusted hands with us.")
+        else:
+            res["reply"] += '\n' + ("Please enter a valid response")
+
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
     return str(res)
 
